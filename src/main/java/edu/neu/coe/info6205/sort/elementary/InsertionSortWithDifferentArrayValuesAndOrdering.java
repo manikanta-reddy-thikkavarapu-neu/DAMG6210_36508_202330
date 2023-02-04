4a180c71ed8154fb6c08ceb9ac5a5268c931c010
Thikkavarapu Manikanta Reddy <t.manikantareddy160@gmail.com>
2023-02-03 21:09:45 -0500

    arrayInsertionSort function created

--- src/main/java/edu/neu/coe/info6205/sort/elementary/InsertionSortWithDifferentArrayValuesAndOrdering.java
+++ src/main/java/edu/neu/coe/info6205/sort/elementary/InsertionSortWithDifferentArrayValuesAndOrdering.java
@@ -1,8 +1,21 @@
 package edu.neu.coe.info6205.sort.elementary;
 
+import edu.neu.coe.info6205.sort.Helper;
+import edu.neu.coe.info6205.sort.HelperFactory;
+import edu.neu.coe.info6205.sort.SortWithHelper;
+import edu.neu.coe.info6205.util.Config;
+
 import java.util.Random;
 
 public class InsertionSortWithDifferentArrayValuesAndOrdering {
+
+    public static void arraySort(int n, Integer[] a) {
+        final Config config = Config.setupConfig("true", "0", "1", "", "");
+        Helper<Integer> helper = HelperFactory.create("InsertionSort", n, config);
+        SortWithHelper<Integer> sorter = new InsertionSort<Integer>(helper);
+        Integer[] ys = sorter.sort(a);
+    }
+
     public static void main(String[] args) {
         int n = 200;
         Integer[] arr = new Integer[n];
@@ -25,18 +38,21 @@ public class InsertionSortWithDifferentArrayValuesAndOrdering {
             Random rand = new Random();
             arr[i] = rand.nextInt(50);
         }
+        arraySort(arr.length, arr);
     }
 
     public static void orderedArray(Integer[] arr) {
         for(int i=0;i<arr.length;i++) {
             arr[i] = i;
         }
+        arraySort(arr.length, arr);
     }
 
     public static void reverseOrderedArray(Integer[] arr) {
         for(int i=arr.length-1;i>=0;i--) {
             arr[arr.length -1 - i] = i;
         }
+        arraySort(arr.length, arr);
     }
 
     public static void partiallyOrderedArray(Integer[] arr) {
@@ -51,5 +67,7 @@ public class InsertionSortWithDifferentArrayValuesAndOrdering {
             Random rand = new Random();
             arr[i] = rand.nextInt(50);
         }
+
+        arraySort(arr.length, arr);
     }
 }
