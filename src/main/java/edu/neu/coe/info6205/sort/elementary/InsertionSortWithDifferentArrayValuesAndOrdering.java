396e111da76b723f64e7d5ca4f70ad8f963e8fbc
Thikkavarapu Manikanta Reddy <t.manikantareddy160@gmail.com>
2023-02-03 21:03:27 -0500

    Remaining Array Order Functions Done

--- src/main/java/edu/neu/coe/info6205/sort/elementary/InsertionSortWithDifferentArrayValuesAndOrdering.java
+++ src/main/java/edu/neu/coe/info6205/sort/elementary/InsertionSortWithDifferentArrayValuesAndOrdering.java
@@ -7,15 +7,47 @@ public class InsertionSortWithDifferentArrayValuesAndOrdering {
         int n = 200;
         Integer[] arr = new Integer[n];
 
-        // Random array values
+        // Random array
         randomArray(arr);
 
-        // 
+        // Ordered array
+        orderedArray(arr);
 
+        // Reverse ordered array
+        reverseOrderedArray(arr);
+
+        // Partially ordered array
+        partiallyOrderedArray(arr);
     }
 
     public static void randomArray(Integer[] arr) {
         for(int i=0;i<arr.length;i++) {
+            Random rand = new Random();
+            arr[i] = rand.nextInt(50);
+        }
+    }
+
+    public static void orderedArray(Integer[] arr) {
+        for(int i=0;i<arr.length;i++) {
+            arr[i] = i;
+        }
+    }
+
+    public static void reverseOrderedArray(Integer[] arr) {
+        for(int i=arr.length-1;i>=0;i--) {
+            arr[arr.length -1 - i] = i;
+        }
+    }
+
+    public static void partiallyOrderedArray(Integer[] arr) {
+
+        // Ordered
+        for(int i=0;i<arr.length/2;i++) {
+            arr[i] = i;
+        }
+
+        //Random
+        for(int i=arr.length/2;i<arr.length;i++) {
             Random rand = new Random();
             arr[i] = rand.nextInt(50);
         }
